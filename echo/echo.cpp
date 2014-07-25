9dcddf438c0117b64e9cdc51243ee4110733ca1b
unknown <qunfengqiu@gmail.com>
2014-07-25 13:19:21 +0800

    add file

---
+++ echo/echo.cpp
@@ -0,0 +1,85 @@
+#include <event2/event.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+
+void EchoServer::start()
+{
+    struct event_base *base;
+    struct evconnlistener * listener;
+    struct sockaddr_in addr;
+    
+    base = event_base_new();
+    
+    if(base == NULL)
+    {
+        std::cout<<"Could not initialize libevent"<<std::endl;
+        return -1;
+    }
+    
+    memset(&addr, 0 ,sizeof(sockaddr_in));
+    addr.sin_port = htons(8080);
+    addr.sin_family = AF_INET;
+    listener = evconnlistener_new_bind(base, EchoServer::onConnection, base, 
+        LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1, (sockaddr*) &addr, sizeof(addr));
+    
+    event_base_dispatch(base);  
+    
+    evlistener_free(listener);
+    event_base_free();
+}
+
+
+void EchoServer::onConnection( struct evconnlistener* listener, evutil_socket_t fd, struct sockaddr* addr, int socklen, void * userdata)
+{
+    struct event_base * base = (struct event_base* ) userdata;
+    struct bufferevent * bevent;
+    
+    bevent = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+    if (!bevent) 
+	{
+        std::cout<< "Error constructing bufferevent!";
+        event_base_loopbreak(base);
+        return;
+    }
+    
+	bufferevent_setcb(bevent, EchoServer::onReadMessage, EchoServer::onWriteMessage, EchoServer::onClose, NULL );
+	
+	bufferevent_enable(bevent, EV_WRITE | EV_READ);
+	//char MESSAGE[] = "Hello, World!\n";
+	//bufferevent_write(bevent, MESSAGE, sizeof(MESSAGE));
+}
+
+void EchoServer::onWriteMessage(struct bufferevent * bevent, void* data)
+{
+	struct evbuffer *output = bufferevent_get_output(bevent);
+	if (evbuffer_get_length(output) == 0) {
+		printf("flushed answer\n");
+		bufferevent_free(bevent);
+	}
+}
+
+void EchoServer::onReadMessage(struct bufferevent * bevent, void* data)
+{
+    //struct evbuffer * input = bufferevent_get_input(bevent);
+	char buf[1024];
+	
+	buffer_read(bevent, buf, sizeof(buf));
+	
+	std::cout<<"received data: "<<buf<<std::endl;
+}
+
+void EchoServer::onClose(struct bufferevent * bevent, short events, void* data)
+{
+    if(events & BEV_EVENT_EOF)
+	{
+	    std::cout<<"Connection closed.\n";
+	}
+	else if( events & BEV_EVENT_ERROR)
+	{
+	    std::cout<<"Got an error on the connection: "<<errno<<std::endl;
+	}
+	bufferevent_free(bevent);
+}
+
---
+++ echo/echo.h
@@ -0,0 +1,23 @@
+#ifndef _ECHO_H_
+#define _ECHO_H_
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+
+// RFC 862
+class EchoServer
+{
+public:
+    EchoServer() {};
+    ~EchoServer() {};
+    
+    void start();
+	
+	static void onConnection( struct evconnlistener* listener, evutil_socket_t fd, struct sockaddr* addr, int socklen, void * userdata);
+	static void onWriteMessage(struct bufferevent * bevent, void* data);
+	static void onReadMessage(struct bufferevent * bevent, void* data);
+	static void onClose(struct bufferevent * bevent, short events,  void* data);
+	
+};
+#endif
